d5544ce080e86f790f699c8052b7994ec8b56cb7
Ricardo <rdlarosa2@gmail.com>
2020-08-30 18:18:57 -0500

    v1_0_1

--- src/com/tienda/Main.java
+++ src/com/tienda/Main.java
@@ -3,8 +3,9 @@ package com.tienda;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
         System.out.println("Primera version");
+        
+        System.out.println("Este es un hotfix");        
 	}
 
 }
